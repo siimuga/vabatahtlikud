f3c207677d85a7bc12aeaac89b9656d344762064
Siim Türk <siimtuerk@gmail.com>
2022-09-08 15:41:14 +0300

    initial commit

---
+++ src/test/java/com/example/vabatahtlikud/VabatahtlikudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vabatahtlikud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VabatahtlikudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
