b0068a3dbd3e166add2336f318102b12be999228
Siim <siim5498@gmail.com>
2022-09-09 00:57:44 +0300

    SIIMT created and added database, filled essential primary tables

---
+++ database/project_data.sql
@@ -0,0 +1,28 @@
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'kõik');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'sport');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'muusika');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'loodus');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'muu');
+
+INSERT INTO public.language (id, name) VALUES (DEFAULT, 'eesti');
+INSERT INTO public.language (id, name) VALUES (DEFAULT, 'vene');
+INSERT INTO public.language (id, name) VALUES (DEFAULT, 'inglise');
+
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Valgamaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Tartumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Harjumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Saaremaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Hiiumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Võrumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Pärnumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Põlvamaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Läänemaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Lääne-Virumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Ida-Virumaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Jõgevamaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Raplamaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Järvamaa');
+INSERT INTO public.county (id, name) VALUES (DEFAULT, 'Viljandimaa');
+
+INSERT INTO public.role (id, name) VALUES (DEFAULT, 'admin');
+INSERT INTO public.role (id, name) VALUES (DEFAULT, 'kasutaja')
