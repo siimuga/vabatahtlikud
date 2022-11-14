8081d9478b459305ddd0556ca7368d1a6fb86d4f
Siim <siim5498@gmail.com>
2022-11-14 20:51:16 +0200

    SIIMT UserRepository update

--- src/main/java/com/example/vabatahtlikud/domain/user/user/UserRepository.java
+++ src/main/java/com/example/vabatahtlikud/domain/user/user/UserRepository.java
@@ -1,12 +1,8 @@
 package com.example.vabatahtlikud.domain.user.user;
 
-import com.example.vabatahtlikud.domain.user.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
